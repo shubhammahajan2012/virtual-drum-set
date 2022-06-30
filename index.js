56f708870bddb986b660cba620b87a38304420b9
Shubham Mahajan <105106134+shubhammahajan2012@users.noreply.github.com>
2022-06-30 22:26:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,92 @@
+var numberOfButton = document.querySelectorAll(".drum").length;
+
+for (i = 0; i < numberOfButton; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+// Detecting Mouse-Button press
+
+  var buttonInnerHTML = this.innerHTML;
+
+  makeSound(buttonInnerHTML);
+
+  drumkitAnimation(buttonInnerHTML);
+
+});
+}
+
+// Detecting keyboard Press
+
+document.addEventListener("keydown", function(event) {
+
+  makeSound(event.key);
+
+  drumkitAnimation(event.key);
+
+});
+
+// Main function
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+    var tom1 = new Audio("sounds/tom-1.mp3");
+    tom1.play();
+      break;
+
+      case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+      case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+      case "e":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+      case "z":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+      case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+      case "l":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+      case "x":
+      var kick = new Audio("sounds/trumpet.mp3");
+      kick.play();
+      break;
+
+    default:
+
+}
+}
+
+// Animation function
+
+function drumkitAnimation(currentkey){
+
+var animatioButton = document.querySelector("."+ currentkey);
+
+animatioButton.classList.add("pressed");
+
+setTimeout(function() {
+
+animatioButton.classList.remove("pressed")
+
+}, 100);
+
+}
